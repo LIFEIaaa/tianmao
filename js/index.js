6464aa271d223c78ed2517aa50f702747b13c42b
LIFEIaaa <1462700432@qq.com>
2018-04-03 00:31:42 +0800

    1

--- js/index.js
+++ js/index.js
@@ -51,6 +51,112 @@
         $(".bottom-btn3").css("transform", "rotate(0)");
     })
 }
+
+//返回顶部
+{
+    $(".totopicon").click(function () {
+
+        $("html,body").animate({scrollTop: 0});
+    });
+}
+//bannerlist
+{
+    $(".nav-item").mouseenter(function () {
+        let index = $(this).index(".nav-item");
+        $(".categoty-item").eq(index).show();
+    })
+        .mouseleave(function () {
+            let index = $(this).index(".nav-item")
+            $(".categoty-item").eq(index).hide();
+        })
+}
+//banner    轮播点
+{
+    $(".lunbo").mouseenter(function () {
+        // let colorarr=["rgb(215,19,59)","rgb(232,232,232)","rgb(232,232,232)","rgb(232,232,232)","rgb(19, 16, 61)"]
+        let index = $(this).index(".lunbo");
+        $(".mainbanner").removeClass("select").eq(index).addClass("select");
+        $(".lunbo").removeClass("select").eq(index).addClass("select");
+        n = index;
+    })
+    let n = 0;
+
+    function move() {
+        n++;
+        if (n === $(".mainbanner").length) {
+            n = 0;
+        }
+        ;
+        if (n < 0) {
+            n = $(".mainbanner").length - 1;
+        }
+        ;
+        $(".mainbanner").removeClass("select").eq(n).addClass("select");
+        $(".lunbo").removeClass("select").eq(n).addClass("select");
+    }
+
+    move();
+    setInterval(move, 3000);
+}
+//天猫超市
+{
+    let n=0;
+    function move() {
+        n++;
+        if(n===$(".todayb").length){
+            n=0;
+        }
+        if(n<0){
+            n=$(".todayb").length-1;
+        }
+        $(".todayb").removeClass("select").eq(n).addClass("select");
+        $(".tabcolor").removeClass("select").eq(n).addClass("select");
+    }
+    move();
+    setInterval(move, 3000);
+}
+//topbar
+{
+    $(window).scroll(function () {
+        let st = $(window).scrollTop();
+        if (st > 700) {
+            $(".topbarbox").show();
+        } else {
+            $(".topbarbox").hide();
+        }
+        if (st > 600) {
+            $(".leftbar").show();
+        } else {
+            $(".leftbar").hide();
+        }
+    });
+}
+//楼层跳转
+{
+    $(".listnav").click(function () {
+        let index = $(this).index(".listnav");
+        let ot = $(".goodsitem").eq(index).offset().top - 200;
+        $("html,body").animate({scrollTop: ot});
+    })
+}
+//监测滚动
+{
+    $(window).scroll(function () {
+        let st = $(window).scrollTop();
+        $(".goodsitem").each(function (index, ele) {
+            if (st >= $(this).offset().top - 200) {
+                $(".leftnavb .listnav").removeClass("active").eq(index).addClass("active");
+            }
+        })
+    })
+}
+//leftbar返回顶部
+{
+    $(".listnavback").click(function () {
+        $("html,body").animate({scrollTop: 0});
+    });
+}
+
 //固定猫
 {
     // let n = 0;
@@ -187,6 +293,12 @@
             .css("opacity", "0")
             .removeClass("leftIn").addClass("leftOut")
     })
+    // $(".bianjiicon").mouseenter(function () {
+    //     $(".rightlogo-xiaoguo1").css("opacity", "1");
+    // })
+    // $(".bianjiicon").mouseleave(function () {
+    //     $(".rightlogo-xiaoguo1").css("opacity", "0");
+    // })
 }
 //固定二维码
 {
@@ -217,90 +329,4 @@
     // })
     // $(fadeIn);
 
-}
-//返回顶部
-{
-    $(".totopicon").click(function () {
-
-        $("html,body").animate({scrollTop: 0});
-    });
-}
-//bannerlist
-{
-    $(".nav-item").mouseenter(function () {
-        let index = $(this).index(".nav-item");
-        $(".categoty-item").eq(index).show();
-    })
-        .mouseleave(function () {
-            let index = $(this).index(".nav-item")
-            $(".categoty-item").eq(index).hide();
-        })
-}
-//轮播点
-{
-    $(".lunbo").mouseenter(function () {
-        let index = $(this).index(".lunbo");
-        $(".mainbanner").removeClass("select").eq(index).addClass("select");
-        $(".lunbo").removeClass("select").eq(index).addClass("select");
-        n = index;
-    })
-    let n = 0;
-
-    function move() {
-        n++;
-        if (n === $(".mainbanner").length) {
-            n = 0;
-        }
-        ;
-        if (n < 0) {
-            n = $(".mainbanner").length - 1;
-        }
-        ;
-        $(".mainbanner").removeClass("select").eq(n).addClass("select");
-        $(".lunbo").removeClass("select").eq(n).addClass("select");
-    }
-
-    move();
-    setInterval(move, 3000);
-}
-//topbar
-{
-    $(window).scroll(function () {
-        let st = $(window).scrollTop();
-        if (st > 700) {
-            $(".topbarbox").show();
-        } else {
-            $(".topbarbox").hide();
-        }
-        if (st > 600) {
-            $(".leftbar").show();
-        } else {
-            $(".leftbar").hide();
-        }
-    });
-}
-//楼层跳转
-{
-    $(".listnav").click(function () {
-        let index = $(this).index(".listnav");
-        let ot = $(".goodsitem").eq(index).offset().top - 200;
-        $("html,body").animate({scrollTop: ot});
-    })
-}
-//监测滚动
-{
-    $(window).scroll(function () {
-        let st = $(window).scrollTop();
-        $(".goodsitem").each(function (index, ele) {
-            if (st >= $(this).offset().top - 200) {
-                $(".leftnavb .listnav").removeClass("active").eq(index).addClass("active");
-            }
-        })
-    })
-}
-//leftbar返回顶部
-{
-    $(".listnavback").click(function () {
-        $("html,body").animate({scrollTop: 0});
-    });
 }
